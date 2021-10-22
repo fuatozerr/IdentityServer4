631bea673d5cd9b3ce35da724371205787864f9b
fuatozerr <fuatozerr23@gmail.com>
2021-10-23 01:36:28 +0300

    mvc client olsuturuldu

---
+++ IdentityServer.Client1/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using IdentityModel.Client;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Client1.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        public ProductController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task<IActionResult> Index()
+        {
+            HttpClient httpClient = new HttpClient();
+
+            var disco = await httpClient.GetDiscoveryDocumentAsync("http://localhost:44397");
+
+            return View();
+        }
+    }
+}
